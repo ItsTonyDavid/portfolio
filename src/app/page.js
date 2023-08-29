c9b0a8c460ad0eb043f678041843ef858683d457
itsTonyDavid <itstonydev@gmail.com>
2023-08-29 14:54:12 -0600

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -35,7 +35,7 @@ export default function Home() {
   }
 
   const handleMessageChange = (newMessage) => {
-    setMessage(newEmail);
+    setMessage(newMessage);
   }
 
   const handleButtonClick = async() => {
